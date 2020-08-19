0120fd142e7e0c8f8bd6c3f8671be7c93c345e98
jfcarocota <jfcarocota@gmail.com>
2020-08-18 18:10:36 -0700

    publish

---
+++ Assets/Scripts/TwitchIRC.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.IO;
+
+public class TwitchIRC : MonoBehaviour
+{
+   TcpClient twitchClient;
+   StreamReader reader;
+   StreamWriter writer;
+
+   [SerializeField]
+   string username, password, channelName;
+
+   [SerializeField]
+   Transform trsCube;
+
+   float rotSpeed = 0;
+
+    void Start()
+    {
+        Connect();
+    }
+
+    void Update()
+    {
+        if(!Connected)
+        {
+            Connect();
+        }
+        trsCube.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+        ReadChat();
+    }
+
+   void Connect()
+   {
+        twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+        writer = new StreamWriter(twitchClient.GetStream());
+        reader = new StreamReader(twitchClient.GetStream());
+
+        writer.WriteLine("PASS " + password);
+        writer.WriteLine("NICK " + username);
+        writer.WriteLine("USER " + username + " 8 * :" + username);
+        writer.WriteLine("JOIN #" + channelName);
+        writer.Flush();
+   }
+
+   void ReadChat()
+   {
+       if(HasMessage)
+       {
+           string message = reader.ReadLine();
+           if(message.Contains("PRIVMSG"))
+           {
+               int splitPoint = message.IndexOf(":", 1);
+               message = message.Substring(splitPoint + 1).ToLower();
+               if(message.Equals("!rot"))
+               {
+                   rotSpeed = 180f;
+               }
+               if(message.Equals("!stop"))
+               {
+                   rotSpeed = 0f;
+               }
+           }
+       }
+   }
+
+   bool Connected => twitchClient.Connected;
+
+   bool HasMessage => twitchClient.Available > 0;
+}
